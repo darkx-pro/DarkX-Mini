3cebb307ec35c24dc1dace89c09ca8b10932a998
darkx-pro <mrxdeveloper2@gmail.com>
2026-04-10 11:10:05 +0300

    Create config.js

---
+++ settings/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    botName: "DarkX-minBot",
+    ownerName: "MrX Dev",
+    ownerNumber: "255775710774",
+    prefix: ".",
+    sessionName: "session",
+    status: {
+        terminal: true, // Weka true ili pairing code itokee kwenye console
+        public: true
+    }
+};
