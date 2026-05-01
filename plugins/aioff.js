c044c80de8e36384535a88a10232d133b3e659cd
darkx-pro <mrxdeveloper2@gmail.com>
2026-05-01 18:50:18 +0300

    Delete plugins directory

--- plugins/aioff.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    command: ['aioff'],
-    isOwner: true,
-    execute: async (sock, m, { reply }) => {
-        global.db.settings.autoAi = false;
-        await reply("📴 *DarkX AI:* Auto-Reply sasa iko OFF!");
-    }
-};
--- plugins/aion.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    command: ['aion'],
-    isOwner: true,
-    execute: async (sock, m, { reply }) => {
-        global.db.settings.autoAi = true;
-        await reply("✅ *DarkX AI:* Auto-Reply sasa iko ON!");
-    }
-};
